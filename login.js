d1c37bf183823f4c06a1458553f9eb1ce563a422
jess6002 <94477527+jess6002@users.noreply.github.com>
2021-11-16 15:04:40 -0300

    login.js

---
+++ login.js
@@ -0,0 +1,16 @@
+function logar(){
+    var usuario = document.getElementById("usuario");
+    var senha = document.getElementById("senha");
+
+    console.log("Usuário: " + usuario.value+" Senha:" + senha.value);
+
+    if(usuario.value == "jessica" && senha.value == "1234"){
+        alert("Usuário autenticado!");
+    }
+    else{
+        alert("Usuário ou senha Invalidos!");
+    }
+}
+
+
+  
